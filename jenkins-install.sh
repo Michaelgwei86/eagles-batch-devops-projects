a25a663d8c88d4d5cdcd873b87163b861f96feeb
Gwei Michael Wawa <101484680+Michaelgwei86@users.noreply.github.com>
2024-10-25 15:13:04 +0100

    Update jenkins-install.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+# Ensure that your software packages are up to date on your instance by using the following command to perform a quick software update:
 sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+# Add the Jenkins repo using the following command:
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+#Import a key file from Jenkins-CI to enable installation from the package:
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
 sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
+
+# Install Java (Amazon Linux 2023):
+sudo dnf install java-17-amazon-corretto -y
+
+sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Install Jenkins
 sudo yum install jenkins -y
+
+# Enable the Jenkins service to start at boot:
 sudo systemctl enable jenkins
+
+# Start Jenkins as a service:
 sudo systemctl start jenkins
 
 # Installing Git
 sudo yum install git -y
-###
-
-# Use The Amazon Linux 2 AMI When Launching The Jenkins VM/EC2 Instance
-# Instance Type: t2.medium or small minimum
-# Open Port (Security Group): 8080 
